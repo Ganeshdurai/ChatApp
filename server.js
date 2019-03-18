764cd559e0d17df59a3bfb130e7c2c5550c98e1b
ganesh durai <duraiganesh123@gmail.com>
2019-03-18 18:56:22 +0530

    commit

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const admin = require('firebase-admin');
 // const functions = require('firebase-functions');
 // mongoose.connect('mongodb://localhost:27017/simpleChat');
 var bodyParser = require('body-parser')
-var server = app.listen(3000)
+var server = app.listen(process.env.PORT || 3000)
 var io = require('socket.io').listen(server);
 // var http = require('http').Server(app);
 // var io = require('socket.io')(http);
