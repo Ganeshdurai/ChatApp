3063a543cbb5d21cabe985ffde82b44dc41e5c11
ganesh durai <duraiganesh123@gmail.com>
2019-03-18 18:59:52 +0530

    added changes

--- server.js
+++ server.js
@@ -37,6 +37,11 @@ io.on('connection', () =>{
 // console.log(mongoose.connection.readyState);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}))
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.get('/messages',function(req,res){
     var messageCollection = firebase.firestore().collection('Messages');
